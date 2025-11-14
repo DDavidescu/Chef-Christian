90698470f68d56d39194816acbc4753f3536ba62
DDavidescu <dbau43@yahoo.com>
2025-11-14 17:01:06 +0200

    Navbar Responsive Update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
-function showSidebar(){
-    const sidebar = document.querySelector('.navbar-list-sidebar');
-    sidebar.style.display = 'flex';
+function toggleSidebar() {
+  const nav = document.querySelector('.navbar');
+  const icon = document.querySelector('.menu-button i');
+  const isOpen = nav.classList.toggle('nav-open');
+
+  if (isOpen) {
+    icon.classList.remove('fa-bars');
+    icon.classList.add('fa-xmark');
+  } else {
+    icon.classList.remove('fa-xmark');
+    icon.classList.add('fa-bars');
+  }
 }
 
-function hideSidebar(){
-    const sidebar = document.querySelector('.navbar-list-sidebar');
-    sidebar.style.display = 'none';
+function hideSidebar() {
+  const nav = document.querySelector('.navbar');
+  const icon = document.querySelector('.menu-button i');
+
+  nav.classList.remove('nav-open');
+  icon.classList.remove('fa-xmark');
+  icon.classList.add('fa-bars');
 }
